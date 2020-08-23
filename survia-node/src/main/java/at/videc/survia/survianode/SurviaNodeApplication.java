fdfbf9608159e4a79c954f92c698f992b3b2b54d
martinvidec <rx451g@gmail.com>
2020-08-23 18:57:36 +0200

    survia-node - create Spring Boot Application

---
+++ survia-node/src/main/java/at/videc/survia/survianode/SurviaNodeApplication.java
@@ -0,0 +1,13 @@
+package at.videc.survia.survianode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurviaNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurviaNodeApplication.class, args);
+	}
+
+}
---
+++ survia-node/src/test/java/at/videc/survia/survianode/SurviaNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package at.videc.survia.survianode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurviaNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
