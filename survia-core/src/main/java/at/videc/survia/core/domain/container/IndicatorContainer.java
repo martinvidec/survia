a4f6269049ca58db6bc22969f82149fa129bd9a9
martinvidec <rx451g@gmail.com>
2020-09-07 00:11:21 +0200

    survia-1 Commit Survia core lib

--- survia-core/src/main/java/at/videc/survia/core/domain/container/IndicatorContainer.java
+++ survia-core/src/main/java/at/videc/survia/core/domain/container/IndicatorContainer.java
@@ -14,4 +14,35 @@ public class IndicatorContainer<T> {
     private IMeasurementUnit measurementUnit;
     private List<ICodedValue<T>> codedValueList;
 
+    public IIndicator<T> getIndicator() {
+        return indicator;
+    }
+
+    public void setIndicator(IIndicator<T> indicator) {
+        this.indicator = indicator;
+    }
+
+    public IDataset getDataset() {
+        return dataset;
+    }
+
+    public void setDataset(IDataset dataset) {
+        this.dataset = dataset;
+    }
+
+    public IMeasurementUnit getMeasurementUnit() {
+        return measurementUnit;
+    }
+
+    public void setMeasurementUnit(IMeasurementUnit measurementUnit) {
+        this.measurementUnit = measurementUnit;
+    }
+
+    public List<ICodedValue<T>> getCodedValueList() {
+        return codedValueList;
+    }
+
+    public void setCodedValueList(List<ICodedValue<T>> codedValueList) {
+        this.codedValueList = codedValueList;
+    }
 }
--- survia-core/src/main/java/at/videc/survia/core/domain/container/ObservationContainer.java
+++ survia-core/src/main/java/at/videc/survia/core/domain/container/ObservationContainer.java
@@ -10,4 +10,36 @@ public class ObservationContainer<T> {
     private IDataset dataset;
     private IIndicator<T> indicator;
     private ICountry country;
+
+    public IObservation<T> getObservation() {
+        return observation;
+    }
+
+    public void setObservation(IObservation<T> observation) {
+        this.observation = observation;
+    }
+
+    public IDataset getDataset() {
+        return dataset;
+    }
+
+    public void setDataset(IDataset dataset) {
+        this.dataset = dataset;
+    }
+
+    public IIndicator<T> getIndicator() {
+        return indicator;
+    }
+
+    public void setIndicator(IIndicator<T> indicator) {
+        this.indicator = indicator;
+    }
+
+    public ICountry getCountry() {
+        return country;
+    }
+
+    public void setCountry(ICountry country) {
+        this.country = country;
+    }
 }
